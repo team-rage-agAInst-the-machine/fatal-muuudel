6cb30de14f7b619280f724f14c125e1eb2cc81c3
Sávio Mendes <savio.souza@asaas.com.br>
2026-05-30 21:08:13 -0300

    feat: migration inicial unificada com schema completo

--- prisma/migrations/20260530214516_add_et_extended_fields/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "eyeCount" INTEGER,
-ADD COLUMN     "forceSensitive" BOOLEAN,
-ADD COLUMN     "iq" INTEGER,
-ADD COLUMN     "locomotion" TEXT,
-ADD COLUMN     "skinColor" TEXT,
-ADD COLUMN     "species" TEXT,
-ADD COLUMN     "starfleetRank" TEXT,
-ADD COLUMN     "towelStatus" TEXT;
